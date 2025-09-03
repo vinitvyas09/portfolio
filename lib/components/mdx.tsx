bba3bed8c02917c9c45974b5d2f04ead615a0d19
Vinit Vyas <vinitvyas09@gmail.com>
2025-09-02 22:48:39 -0700

    Add mdxComponents file to export MDX components for improved modularity and organization in rendering.

---
+++ lib/components/mdx.tsx
@@ -0,0 +1,13 @@
+import { Intuition } from "./mdx/Intuition"
+import { Math } from "./mdx/Math"
+import { Code } from "./mdx/Code"
+import { Notebook } from "./mdx/Notebook"
+import { References } from "./mdx/References"
+
+export const mdxComponents = {
+  Intuition,
+  Math,
+  Code,
+  Notebook,
+  References,
+}
