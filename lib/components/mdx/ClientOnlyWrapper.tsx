d274fbaafa5806116ae2055fdfdcffe83cf13c4e
Vinit Vyas <vinitvyas09@gmail.com>
2025-09-18 19:18:26 -0700

    Add ClientOnlyWrapper component for dynamic imports of framer-motion components. This new file enables server-side rendering to be disabled for NeuronVsPerceptron, PerceptronContinuum, and InfoBox, enhancing performance and user experience.

---
+++ lib/components/mdx/ClientOnlyWrapper.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import dynamic from 'next/dynamic';
+
+// Dynamically import components that use framer-motion with SSR disabled
+export const NeuronVsPerceptron = dynamic(
+  () => import('../../../components/framer/neuron-vs-perceptron'),
+  { ssr: false }
+);
+
+export const PerceptronContinuum = dynamic(
+  () => import('../../../components/framer/perceptron-morph'),
+  { ssr: false }
+);
+
+export const InfoBox = dynamic(
+  () => import('../../../components/framer/info-box'),
+  { ssr: false }
+);
